853fa16e4cc33011e33ee173586a534125760cfb
Aman Kumar <savverma98@gmail.com>
2022-06-26 22:55:53 +0530

    java first program

---
+++ src/Blueprint.java
@@ -0,0 +1,9 @@
+public class Blueprint {
+    public static void main(String[] args){
+        System.out.println("Hello Aman kumar  this is my first program in java");
+        System.out.println("hii yes this my first progrmer");
+        System.out.println("welocme ");
+        System.out.println("hello");
+    }
+
+}
